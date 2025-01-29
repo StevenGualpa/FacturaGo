0746f1754221359d55b2e991b7a0a907f42a0fff
Steven Gualpa <68717276+StevenGualpa@users.noreply.github.com>
2025-01-29 13:10:26 -0500

    Descripción breve de los cambios realizados

--- main.go
+++ main.go
@@ -1,1 +1,61 @@
 package FacGo
+
+import (
+	"FacGo/models"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/spf13/viper"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+
+	"log"
+)
+
+func main() {
+	app := fiber.New() // Inicia la aplicación Fiber
+
+	// Configura el middleware CORS para permitir solicitudes de cualquier origen
+	app.Use(cors.New())
+
+	config := viper.New() // Inicia Viper para la configuración
+
+	// Configura Viper para leer variables de entorno
+	config.AutomaticEnv()
+
+	config.SetDefault("APP_PORT", "3000")
+	config.SetDefault("APP_ENV", "development")
+	config.SetConfigName("config") // Nombre del archivo de configuración sin la extensión
+	config.SetConfigType("env")    // Extensión del archivo de configuración
+	config.AddConfigPath(".")      // Ubicación del archivo de configuración
+	config.AddConfigPath("/etc/secrets/")
+
+	// Intenta leer el archivo de configuración
+	if err := config.ReadInConfig(); err != nil {
+		log.Printf("Advertencia: No se pudo leer el archivo de configuración. %v", err)
+	}
+
+	// Establece la cadena DSN para la conexión a la base de datos
+	dsn := "host=ep-lingering-snowflake-a5j9m53w.us-east-2.aws.neon.tech user=stevengualpa password=VamLyM2btnd4 dbname=carinosabd port=5432 sslmode=verify-full"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Error al conectar con la base de datos: %v", err)
+	}
+
+	//NUEVOS CAMBIOS
+	// Automigración para el modelo User
+	if err := db.AutoMigrate(&models.User{}); err != nil {
+		log.Fatalf("Error en la automigración: %v", err)
+	}
+
+	// Define una ruta de bienvenida
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("¡Hola, Mundo!")
+	})
+
+	// Inicia el servidor en el puerto configurado
+	port := config.GetString("APP_PORT")
+	log.Printf("Servidor iniciado en el puerto %s", port)
+	if err := app.Listen(":" + port); err != nil {
+		log.Fatalf("Error al iniciar el servidor: %v", err)
+	}
+}
---
+++ models/user.go
@@ -0,0 +1,35 @@
+package models
+
+import "gorm.io/gorm"
+
+// User representa un usuario general con información básica.
+type User struct {
+	gorm.Model
+	FirstName   string    `json:"firstname"`
+	LastName    string    `json:"lastname"`
+	Email       string    `json:"email" gorm:"unique"`
+	Password    string    `json:"password"`
+	BirthDate   string    `json:"birthdate"`
+	Gender      string    `json:"gender"`
+	PhoneNumber string    `json:"phone"`
+	Roles       string    `json:"roles"`                // Define el rol del usuario ("cuidador", "paciente", "admin", etc.).
+	Cedula      string    `json:"cedula" gorm:"unique"` // Campo cédula agregado aquí
+	Cuidador    *Cuidador `gorm:"foreignKey:UserID"`
+	Paciente    *Paciente `gorm:"foreignKey:UserID"`
+}
+
+// Cuidador contiene información específica para usuarios con el rol de cuidador.
+type Cuidador struct {
+	gorm.Model
+	UserID   uint   `gorm:"uniqueIndex"` // Clave foránea que apunta a User.
+	Relacion string `json:"relacion"`
+	User     User   `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+}
+
+// Paciente contiene información específica para usuarios con el rol de paciente.
+type Paciente struct {
+	gorm.Model
+	UserID           uint   `gorm:"uniqueIndex"` // Clave foránea que apunta a User.
+	NumeroEmergencia string `json:"numeroEmergencia"`
+	User             User   `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+}
